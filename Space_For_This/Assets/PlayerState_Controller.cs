f4250fa65cd9f72808d13780ca58873be7bd4937
Rob Carter <robbie.carter@gmail.com>
2017-06-08 21:27:56 +0300

    swarm controls start

---
+++ Space_For_This/Assets/PlayerState_Controller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerState_Controller : MonoBehaviour {
+
+    public Slider healthSlider;
+
+    public float currentHealth;
+    public float maxHealth;
+
+	// Use this for initialization
+	void Awake () {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void damagePlayer(float damage) {
+
+        if (currentHealth - damage < 0)
+        {
+            currentHealth = 0;
+        }
+        else {
+            currentHealth -= damage;
+        }
+
+        healthSlider.value = currentHealth;
+
+    }
+}
+
---
+++ Space_For_This/Assets/Swarm.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Swarm : MonoBehaviour {
+	List<Ship> swarmShips;
+	List<SwarmPathAction> swarmActions;
+
+	public Swarm(List<Ship> ships, List<SwarmPathAction> actions){
+		this.swarmShips = ships;
+		this.swarmActions = actions;
+	}
+
+	public Swarm GenerateTestSwarm(){
+		List<SwarmPathAction> actions = new List<SwarmPathAction> ();
+		List<Ship> ships = new List<Ship> ();
+
+		actions.Add (
+			new SwarmPathAction (swarmActionType.move, new SwarmActionDetails (Vector3.zero, 3))
+		);
+
+		return new Swarm (ships, actions);
+	}
+}
+
+public class SwarmPathAction {
+	swarmActionType actionType;
+	SwarmActionDetails actionDetails;
+
+	public SwarmPathAction(swarmActionType type, SwarmActionDetails details)
+	{
+		this.actionType = type;
+		this.actionDetails = details;
+	}
+}
+
+public class SwarmActionDetails{
+	Vector3 moveTarget;
+	float moveSpeed;
+
+	public SwarmActionDetails(Vector3 target, float speed = 0)
+	{
+		this.moveTarget = target;
+		this.moveSpeed = speed;
+	}
+}
+
+public enum swarmActionType { move, fire, formation } 
